6cbcf6804e3564fd5dcdc4e908073e07490655fe
ramachandrasahoo <ramachandrasahoo970@gmail.com>
2025-07-20 07:31:22 +0530

    S

---
+++ script.js
@@ -0,0 +1,11 @@
+// 1 write a programm that print "Hello World!"
+console.log("Hello World!");
+
+// 2 write a function that takes 2 number as input and print their sum
+function sum (a,b){
+    let sum = a + b ;
+    console.log("sum is" + " " + sum);
+};
+sum(2,3);
+
+// 3 
